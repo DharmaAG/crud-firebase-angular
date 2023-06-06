db8f896a14b8bf8831c8ed047c6019136e39c8ec
DharmaAG <dharma.g@aluno.ifsc.edu.br>
2023-06-06 15:09:12 -0300

    alteração
    add bootstrap
    add environment.ts

---
+++ src/environment.ts
@@ -0,0 +1,15 @@
+
+export const environment = {
+
+  production: false,
+
+  firebaseConfig : {
+  apiKey: "AIzaSyCKaFM8RGkyY_rmG8Z9y17M-2IVEjvLmEk",
+  authDomain: "crud-angular-firebase-61cf8.firebaseapp.com",
+  databaseURL: "https://crud-angular-firebase-61cf8-default-rtdb.firebaseio.com",
+  projectId: "crud-angular-firebase-61cf8",
+  storageBucket: "crud-angular-firebase-61cf8.appspot.com",
+  messagingSenderId: "915361724948",
+  appId: "1:915361724948:web:84e88ad11107f88ad84501"
+  }
+};
