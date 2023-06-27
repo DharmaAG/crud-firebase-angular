ee2fdbcc61cf5a55c94d9e6f99b7088ad4490702
DharmaAG <dharma.g@aluno.ifsc.edu.br>
2023-06-27 16:41:21 -0300

    add emoji

--- src/environment.ts
+++ src/environment.ts
@@ -4,12 +4,13 @@ export const environment = {
   production: false,
 
   firebaseConfig : {
-  apiKey: "AIzaSyCKaFM8RGkyY_rmG8Z9y17M-2IVEjvLmEk",
-  authDomain: "crud-angular-firebase-61cf8.firebaseapp.com",
-  databaseURL: "https://crud-angular-firebase-61cf8-default-rtdb.firebaseio.com",
-  projectId: "crud-angular-firebase-61cf8",
-  storageBucket: "crud-angular-firebase-61cf8.appspot.com",
-  messagingSenderId: "915361724948",
-  appId: "1:915361724948:web:84e88ad11107f88ad84501"
+  apiKey: "AIzaSyA9yHyEPUYt3SVPIW6gG7QIwnXZ6efBqmc",
+  authDomain: "comida-firebase.firebaseapp.com",
+  databaseURL: "https://comida-firebase-default-rtdb.firebaseio.com",
+  projectId: "comida-firebase",
+  storageBucket: "comida-firebase.appspot.com",
+  messagingSenderId: "575729367836",
+  appId: "1:575729367836:web:bb4d8cf7bb48a3a36918d9",
+  measurementId: "G-K1G9TGKCNW"
   }
 };
